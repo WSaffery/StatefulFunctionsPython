bdd9d7d5d5629dcdce2fce7cff745019d5795598
WSaffery <William.Saffery@gmail.com>
2021-11-11 13:49:05 +1100

    change this to self

--- examples.py
+++ examples.py
@@ -1,30 +1,30 @@
 from state_func import *
 
 @state_func
-def test(this, a, b,c):
+def test(self, a, b,c):
     try:
-        this.count += 1
+        self.count += 1
     except:
-        this.count = 0
-    print(a,b,c, this.count)
+        self.count = 0
+    print(a,b,c, self.count)
     
 test("hi", "hello", "goodbye") # hi hello goodbye 0
 test("hi", "hello", "goodbye") # hi hello goodbye 1
 test("hi", "hello", "goodbye") # hi hello goodbye 2
 
 @state_func_e({"count":0})
-def test2(this, a, b,c):
-    print(a,b,c, this.count)
-    this.count += 1 
+def test2(self, a, b,c):
+    print(a,b,c, self.count)
+    self.count += 1 
     
 test2("hi", "hello", "goodbye") # hi hello goodbye 0
 test2("hi", "hello", "goodbye") # hi hello goodbye 1
 test2("hi", "hello", "goodbye") # hi hello goodbye 2
 
 @state_func_i(test2)
-def test3(this, a, b,c):
-    print(a,b,c, this.count)
-    this.count += 1 
+def test3(self, a, b,c):
+    print(a,b,c, self.count)
+    self.count += 1 
     
 test3("hi", "hello", "goodbye") # hi hello goodbye 0
 test3("hi", "hello", "goodbye") # hi hello goodbye 1
