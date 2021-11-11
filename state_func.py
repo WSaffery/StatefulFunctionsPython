816001393a3f5bbd5a60b7674ca4c7e0519c12dc
WSaffery <William.Saffery@gmail.com>
2021-11-11 13:46:49 +1100

    experimental inheritance

--- state_func.py
+++ state_func.py
@@ -1,3 +1,5 @@
+import copy
+
 class stateFunc:
     def __init__(self, body):
         self.body = body
@@ -6,22 +8,58 @@ class stateFunc:
         self.body(self, *args)
 
 class stateFuncE(stateFunc):
-    def __init__(self, kattr, body):
+    def __init__(self, body, kattr):
+        self.base_kattr = {}
         self.assign(kattr)
         super().__init__(body)
     
     def assign(self, kattr):
+        self.base_kattr.update(copy.deepcopy(kattr))
         for k, v in kattr.items():
             setattr(self, k, v)
             
+class stateFuncM(stateFuncE):
+    def __init__(self, body, sources):
+        self.base_kattr = {}
+        for s in sources:
+            self.assign(s)
+        self.body = body
+        
+def check_dictlike(dictlike):
+    try:
+        dictlike.items()
+        return True
+    except:
+        return False
+    
+class stateFuncI(stateFuncM):
+    def __init__(self, body, sources):
+        changed_sources = list(sources)
+        first_source = sources[0]
+        if hasattr(first_source, '__self__'):
+            upper = first_source.__self__
+            # print(upper)
+            if not issubclass(type(upper),stateFunc):
+                print("not")
+                raise Exception("Invalid inheritence")
+            else:
+                changed_sources[0] = upper.base_kattr
+        super().__init__(body, changed_sources)
+            
     
 def state_func(function):
-        sf = stateFunc(function)
-        return sf.body_run
+    sf = stateFunc(function)
+    return sf.body_run
 
 def state_func_e(kattr):
     def decorator(function):
-        sf_e = stateFuncE(kattr, function)
+        sf_e = stateFuncE(function, kattr)
+        return sf_e.body_run
+    return decorator
+    
+def state_func_i(*sources):
+    def decorator(function):
+        sf_e = stateFuncI(function, sources)
         return sf_e.body_run
     return decorator
     
@@ -46,3 +84,12 @@ def test2(this, a, b,c):
 test2("hi", "hello", "goodbye") # hi hello goodbye 0
 test2("hi", "hello", "goodbye") # hi hello goodbye 1
 test2("hi", "hello", "goodbye") # hi hello goodbye 2
+
+@state_func_i(test2)
+def test3(this, a, b,c):
+    print(a,b,c, this.count)
+    this.count += 1 
+    
+test3("hi", "hello", "goodbye") # hi hello goodbye 0
+test3("hi", "hello", "goodbye") # hi hello goodbye 1
+test3("hi", "hello", "goodbye") # hi hello goodbye 2
